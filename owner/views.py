93931b9e774372cac2b572b0801b3c22203eb292
jonathankun <jonathanriveraroman@gmail.com>
2022-12-05 13:35:57 -0500

    template y orms

--- owner/views.py
+++ owner/views.py
@@ -1,14 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
+from owner.models import Owner
 
 
 def owner_list(request):
     data_context = {
-        'nombre_owner': 'Kevin Melgarejo',
-        'edad': 18,
+        'nombre_owner': 'Katty Paredes',
+        'edad': 24,
         'pais': 'Perú',
+        'dni': '12345678',
         'vigente': True
     }
 
-    return render(request, 'owner/owner_list.html', context={'data': data_context})
+    owners = [
+        {
+            'nombre_owner': 'Daniel Ordenado',
+            'edad': 20,
+            'pais': 'Perú',
+            'dni': '12345678',
+            'vigente': True
+        },
+        {
+            'nombre_owner': 'Katty Paredes',
+            'edad': 24,
+            'pais': 'Brasil',
+            'dni': '43340690',
+            'vigente': True
+        },
+        {
+            'nombre_owner': 'Carlos Carbajal',
+            'edad': 28,
+            'pais': 'Perú',
+            'dni': '42305699',
+            'vigente': True
+        },
+        {
+            'nombre_owner': 'Juan Ríos',
+            'edad': 34,
+            'pais': 'México',
+            'dni': '11395600',
+            'vigente': True
+        }
+    ]
+
+
+    """Crear un objeto en la Base de Datos para la tabla de owner"""
+    p = Owner(nombre="Rossmery",pais="España", edad=21)
+    p.save() # Guarda el registro en la B.D.
+
+    p.nombre = "Beatriz"
+    p.save()
+
+    return render(request, 'owner/owner_list.html', context={'data': owners})
